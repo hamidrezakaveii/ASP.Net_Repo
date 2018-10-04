1ff8a8b6b8c4f7038f0f4f2785e68a1c0691fb31
Hami Kaveii <hamidreza.kaveii@gmail.com>
2018-10-04 17:43:00 -0400

    changes

--- ExamFinal/ExamFinal/Models/Frais.cs
+++ ExamFinal/ExamFinal/Models/Frais.cs
@@ -8,26 +8,7 @@ namespace ExamFinal.Models
 {
     public class Frais
     {
-        private string v1;
-        private string v2;
-        private string v3;
-        private int v4;
-        private string v5;
-        private double v6;
-        private string v7;
-        private string v8;
 
-        public Frais(string v1, string v2, string v3, int v4, string v5, double v6, string v7, string v8)
-        {
-            this.v1 = v1;
-            this.v2 = v2;
-            this.v3 = v3;
-            this.v4 = v4;
-            this.v5 = v5;
-            this.v6 = v6;
-            this.v7 = v7;
-            this.v8 = v8;
-        }
 
         [Required(ErrorMessage = "Le nom est obligatoire")]
         public string Nom { get; set; }
@@ -57,6 +38,19 @@ namespace ExamFinal.Models
 
         [EmailAddress]
         public string Courriel { get; set; }
+
+
+        public Frais(string v1, string v2, string v3, int v4, string v5, double v6, string v7, string v8)
+        {
+            this.Nom = v1;
+            this.Prenom = v2;
+            this.Date = v3;
+            this.Kilometre = v4;
+            this.Ville = v5;
+            this.FraisDeResturant = v6;
+            this.CodePostal = v7;
+            this.Courriel = v8;
+        }
 
 
     }
